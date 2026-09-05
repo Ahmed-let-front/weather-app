c5c49928dbe16108f560b748708babbd68bb77fb
Ahmed <letcosdgp@gmail.com>
2026-09-05 12:02:38 +0300

    add import for images

--- src/js/view/weatherStats.js
+++ src/js/view/weatherStats.js
@@ -1,11 +1,11 @@
-import iconDrizzle from "../assets/images/icon-drizzle.webp";
-import iconFog from "../assets/images/icon-fog.webp";
-import iconOvercast from "../assets/images/icon-overcast.webp";
-import iconPartlyCloudy from "../assets/images/icon-partly-cloudy.webp";
-import iconRain from "../assets/images/icon-rain.webp";
-import iconSnow from "../assets/images/icon-snow.webp";
-import iconStorm from "../assets/images/icon-storm.webp";
-import iconSunny from "../assets/images/icon-sunny.webp";
+import iconDrizzle from "../../assets/images/icon-drizzle.webp";
+import iconFog from "../../assets/images/icon-fog.webp";
+import iconOvercast from "../../assets/images/icon-overcast.webp";
+import iconPartlyCloudy from "../../assets/images/icon-partly-cloudy.webp";
+import iconRain from "../../assets/images/icon-rain.webp";
+import iconSnow from "../../assets/images/icon-snow.webp";
+import iconStorm from "../../assets/images/icon-storm.webp";
+import iconSunny from "../../assets/images/icon-sunny.webp";
 class WeatherStats {
   #weatherIconMap = {
     0: iconSunny,
